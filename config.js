93bdc91fa9cff354ce734e943e59b226ea536284
lilzinn <80847791+lilzinn@users.noreply.github.com>
2021-08-12 14:46:27 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const ownerNumber = ["557388843631@s.whatsapp.net"] 
 // mude somente o numero e não tire o @s.whatsapp.net
 
-const OriginalOwner = '557388843631' 
+const OriginalOwner = '5573888436311' 
 // mude para seu numero
 
 const callbotmsgs = ['orion', 'brizas']
